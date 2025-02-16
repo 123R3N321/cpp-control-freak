a8a5569c773890d15a555c83e1066b0514908165
123R3N321 <jr5887@nyu.edu>
2025-02-15 23:38:41 -0500

    Modify .yml
    Limit clang formatting to ony main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
-    std::cout<<"this is how a control freak manages their repo!"<<std::endl;
+    std::cout << "this is how a control freak manages their repo!" << std::endl;
     return 0;
 }
 
