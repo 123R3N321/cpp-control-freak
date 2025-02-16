721dd0990455c23e52bc7ab6b77824bdf5e51cac
Joe Braha <jbraha@nyu.edu>
2025-02-16 13:32:26 -0500

    add vcpkg to project
    to setup, run `source setup-vcpkg.sh` from project root directory

---
+++ setup_vcpkg.sh
@@ -0,0 +1,16 @@
+# source this script from the project root directory
+# source setup_vcpkg.sh
+
+# download and bootstrap vcpkg if not yet set up
+if [ ! -d vcpkg ]
+then
+  git clone https://github.com/microsoft/vcpkg.git
+  cd vcpkg && ./bootstrap-vcpkg.sh
+  cd -
+fi
+
+# set env var for vcpkg dir and add it to PATH
+if [ -z "$VCPKG_ROOT" ]; then
+  export VCPKG_ROOT="$(pwd)/vcpkg"
+  export PATH=$VCPKG_ROOT:$PATH
+fi
