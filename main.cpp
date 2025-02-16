7dba59ece9e1de020458927c68894281fa3d6367
123R3N321 <jr5887@nyu.edu>
2025-02-15 23:40:34 -0500

    Update main.cpp
    clear it out to pass clamg format

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-    std::cout << "this is how a control freak manages their repo!" << std::endl;
-    return 0;
-}
-
