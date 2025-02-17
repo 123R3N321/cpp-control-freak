6f1ff9870946e977b4edee7c275e6b17ec78f8be
Joe Braha <jbraha@nyu.edu>
2025-02-16 22:17:45 -0500

    add end to end test

---
+++ tests/e2e_test.cpp
@@ -0,0 +1,37 @@
+#include "gtest/gtest.h"
+#include "../Components/calculator.hpp"
+#include <fstream>
+#include <sstream>
+
+class E2ETest : public ::testing::Test {
+protected:
+    Logger logger;
+    Notifier notifier{10}; // Set threshold to 10
+    Calculator calculator{logger, notifier};
+
+    std::string captureLog() {
+        std::ifstream logFile("calculator.log");
+        std::stringstream buffer;
+        buffer << logFile.rdbuf();
+        return buffer.str();
+    }
+};
+
+TEST_F(E2ETest, AddTest) {
+    testing::internal::CaptureStdout();
+
+    // verify addition
+    int result = calculator.add(10, 5);
+    EXPECT_DOUBLE_EQ(result, 15);
+
+    // verify log output
+    std::string logContent = captureLog();
+    EXPECT_NE(logContent.find("Operation: Addition"), std::string::npos);
+    EXPECT_NE(logContent.find("Operands: 2, 6"), std::string::npos);
+    EXPECT_NE(logContent.find("Result: 8"), std::string::npos);
+
+    // verify notification
+    std::string captured_output = testing::internal::GetCapturedStdout();
+    EXPECT_FALSE(captured_output.empty());
+}
+
